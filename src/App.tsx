4b3a013839314ea2399dab334a479c96837d5ef3
alexbelz123 <belz.alexx@gmail.com>
2023-10-04 14:05:06 +0300

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mavryk Dynamics template</p>
+        <p>Mavryk Dynamics template (Dev)</p>
       </header>
     </div>
   );
