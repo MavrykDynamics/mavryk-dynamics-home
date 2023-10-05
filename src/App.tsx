8583e84dcae87187a2813f87c5fc5993bfb764ff
alexbelz123 <belz.alexx@gmail.com>
2023-10-05 12:21:02 +0300

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'styles/grid.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Mavryk Dynamics template (Staging)</p>
       </header>
     </div>
