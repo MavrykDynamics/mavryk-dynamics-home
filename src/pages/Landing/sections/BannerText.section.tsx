118cfe0803c5b959c7aecf10f7c1f158d080af55
Sam Morrow <smorrow32@gmail.com>
2023-11-02 15:24:57 +0200

    MAV-002 #time 5m /reverted one-liner text

--- src/pages/Landing/sections/BannerText.section.tsx
+++ src/pages/Landing/sections/BannerText.section.tsx
@@ -24,7 +24,7 @@ export const BannerTextSection = () => {
           alt="banner-text"
         />
         <p className={styles.bannerParagraph}>
-          Where visionary pioneers spearhead the vanguard of web3 innovation
+          Real assets, Real people, Real solutions. Building a smarter, more secure future. Blockchain, Done Better.
         </p>
       </div>
     </section>
